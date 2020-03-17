8c9a253aada32c734095cf0c6e7f877c39aed7fb
Nobu <rockmusic2090@gmail.com>
2020-03-16 17:00:57 -0700

    Created Book Type for graphQL

--- schema/schema.js
+++ schema/schema.js
@@ -1,1 +1,12 @@
 const graphql = require("graphql");
+
+const { GraphQLObjectType, GraphQLString, GraphQLInt } = graphql;
+
+const BookType = new GraphQLObjectType({
+  name: "Book",
+  fileds: () => ({
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    genre: { type: GraphQLString }
+  })
+});
