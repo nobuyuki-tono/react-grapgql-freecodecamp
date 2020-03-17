0518d24409d399f4632a1b7fbb37f30d134b16ad
Nobu <rockmusic2090@gmail.com>
2020-03-17 14:43:45 -0700

    Connected to MongoDB

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const schema = require("./schema/schema");
+const mongoose = require("mongoose");
+require("dotenv").config({ path: "./config/local.env" });
 
 const app = express();
+
+// Connect to MongoDB
+
+mongoose.connect(
+  process.env.MONGODB_URI,
+  {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+  },
+  () => {
+    console.log("Connected to DB");
+  }
+);
 
 app.use(
   "/graphql",
