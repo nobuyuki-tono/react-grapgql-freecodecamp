41bcafbddae7bc3c9188aa299ec885224d55e82b
Nobu <rockmusic2090@gmail.com>
2020-03-17 16:50:15 -0700

    Created BookList component

---
+++ client/src/components/BookList.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const BookList = () => {
+  return (
+    <div>
+      <ui id="book-list">
+        <li>Book name</li>
+      </ui>
+    </div>
+  );
+};
+
+export default BookList;
