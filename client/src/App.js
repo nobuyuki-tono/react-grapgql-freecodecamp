4e4edfbc1d24c532800ed5dc7f8a2020a0eaf010
Nobu <rockmusic2090@gmail.com>
2020-03-18 10:23:21 -0700

    Added graphql npm package

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,24 @@
 import React from "react";
+import ApolloClient from "apllo-boost";
+import { ApolloProvider } from "@apollo/react-hooks";
+
+import BooKList from "./components/BookList";
 
 import "./App.css";
 
+// Apollo Client setup
+const client = new ApolloClient({
+  uri: "http://localhost:5000/graphql"
+});
+
 function App() {
   return (
-    <div className="App">
-      <h1>HELLO</h1>
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+        <h1>Book List</h1>
+        <BooKList />
+      </div>
+    </ApolloProvider>
   );
 }
 
--- client/src/components/BookList.jsx
+++ client/src/components/BookList.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { gql } from "apollo-boost";
 
 const BookList = () => {
   return (
