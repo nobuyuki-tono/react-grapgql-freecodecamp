157ed277cea530095d60dc62b2a626b7bd6a1941
Nobu <rockmusic2090@gmail.com>
2020-03-18 10:29:49 -0700

    Created getBookQuery

--- client/src/components/BookList.jsx
+++ client/src/components/BookList.jsx
@@ -1,6 +1,15 @@
 import React from "react";
 import { gql } from "apollo-boost";
 
+const getBooksQuery = gql`
+  {
+    books {
+      name
+      id
+    }
+  }
+`;
+
 const BookList = () => {
   return (
     <div>
