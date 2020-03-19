8090f434382ecb175e82aac73bb7b5d5256eea19
Nobu <rockmusic2090@gmail.com>
2020-03-19 10:59:32 -0700

    Created displayBookDetails function

--- client/src/components/BookDetails.jsx
+++ client/src/components/BookDetails.jsx
@@ -2,17 +2,37 @@ import React from "react";
 import { useQuery } from "@apollo/react-hooks";
 import { getBookQuery } from "../queries/queries";
 
-const BookDetails = () => {
-  const { loading, error, data } = useQuery(getBookQuery);
+const BookDetails = ({ bookId }) => {
+  const { loading, error, data } = useQuery(getBookQuery, {
+    variables: { id: bookId }
+  });
+
+  const displayBookDetails = () => {
+    const { book } = data;
+
+    if (book) {
+      return (
+        <div>
+          <h2>{book.name}</h2>
+          <p>{book.genre}</p>
+          <p>{book.author.name}</p>
+          <p>All books by this author</p>
+          <ul className="other-books">
+            {book.auhtor.books.map(book => {
+              return <li key={book.id}>{book.name}</li>;
+            })}
+          </ul>
+        </div>
+      );
+    } else {
+      return <div>No book selected</div>;
+    }
+  };
 
   if (loading) return <p>Loading...</p>;
   // if (error) return <p>Error</p>;
 
-  return (
-    <div id="book-details">
-      <p>Output book details here</p>
-    </div>
-  );
+  return <div id="book-details">{displayBookDetails()}</div>;
 };
 
 export default BookDetails;
